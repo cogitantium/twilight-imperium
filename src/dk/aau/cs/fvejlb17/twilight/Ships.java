60962eff8696b43c939d4d3a6d629617b6515c18
cogitantium <falke.cs@gmail.com>
2018-05-03 14:12:19 +0200

    added framework for Units and Ships

---
+++ src/dk/aau/cs/fvejlb17/twilight/Ships.java
@@ -0,0 +1,10 @@
+package dk.aau.cs.fvejlb17.twilight;
+
+public abstract class Ships {
+
+    private Player owner;
+    private int resourceCost;
+    private int combatValue;
+    private int movementSpeed;
+    private int capacity;
+}
---
+++ src/dk/aau/cs/fvejlb17/twilight/Units.java
@@ -0,0 +1,10 @@
+package dk.aau.cs.fvejlb17.twilight;
+
+public interface Units {
+
+    Player getOwner();
+    int getResourceCost();
+    int getCombatValue();
+    int getMovementSpeed();
+    int getCapacity();
+}
