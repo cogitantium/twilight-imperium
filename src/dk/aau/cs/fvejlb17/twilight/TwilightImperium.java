03a540306b422251c6d67a2197dafd6c61a6b109
cogitantium <falke.cs@gmail.com>
2018-05-05 16:43:29 +0200

    added explicit creation of required objects

--- src/dk/aau/cs/fvejlb17/twilight/TwilightImperium.java
+++ src/dk/aau/cs/fvejlb17/twilight/TwilightImperium.java
@@ -1,7 +1,11 @@
 package dk.aau.cs.fvejlb17.twilight;
 
+import dk.aau.cs.fvejlb17.twilight.galaxies.Galaxy;
 import dk.aau.cs.fvejlb17.twilight.planets.Planet;
+import dk.aau.cs.fvejlb17.twilight.planets.PlanetList;
 import dk.aau.cs.fvejlb17.twilight.players.Player;
+import dk.aau.cs.fvejlb17.twilight.systems.SystemTile;
+import dk.aau.cs.fvejlb17.twilight.systems.SystemTileList;
 import dk.aau.cs.fvejlb17.twilight.units.*;
 
 public class TwilightImperium {
@@ -29,7 +33,7 @@ public class TwilightImperium {
         UnitList unitList02 = new UnitList();
         unitList02.add(cruiser01); unitList02.add(cruiser02); unitList02.add(carrier01);
 
-        //creating planets with sensible resource production
+        //create planets with sensible resource production
         Planet mecatolRex = new Planet("Mecatol Rex", 6);
         Planet vegaMinor = new Planet("Vega Minor", 2);
         Planet vegaMajor = new Planet("Vega Major", 5);
@@ -38,5 +42,43 @@ public class TwilightImperium {
         Planet rigelTwo= new Planet("Rigel II", 4);
         Planet mirage= new Planet("Mirage", 3);
 
+        //create PlanetLists
+        PlanetList centerPlanets = new PlanetList(); centerPlanets.add(mecatolRex);
+        PlanetList northPlanets = new PlanetList(); northPlanets.add(vegaMinor); northPlanets.add(vegaMajor);
+        //omitting PlanetList northEastPlanets = new PlanetList();
+        PlanetList southEastPlanets = new PlanetList(); southEastPlanets.add(industrex);
+        PlanetList southPlanets = new PlanetList(); southPlanets.add(rigelOne); southPlanets.add(rigelTwo);
+        // omitting PlanetList southWestPlanets = new PlanetList();
+        PlanetList northWestPlanets = new PlanetList(); northWestPlanets.add(mirage);
+
+        //create SystemTiles
+        SystemTile centerSystem = new SystemTile(unitList01, centerPlanets);
+        SystemTile northSystem = new SystemTile(unitList02, northPlanets);
+        SystemTile northEastSystem = new SystemTile();
+        SystemTile southEastSystem = new SystemTile(southEastPlanets);
+        SystemTile southSystem = new SystemTile(southPlanets);
+        SystemTile southWestSystem = new SystemTile();
+        SystemTile northWestSystem = new SystemTile(northWestPlanets);
+
+        //create SystemTileList and add all SystemTiles
+        SystemTileList systemTileList01 = new SystemTileList();
+        systemTileList01.add(centerSystem);
+        systemTileList01.add(northSystem);
+        systemTileList01.add(northEastSystem);
+        systemTileList01.add(southEastSystem);
+        systemTileList01.add(southSystem);
+        systemTileList01.add(southWestSystem);
+        systemTileList01.add(northWestSystem);
+
+        //create galaxy
+        Galaxy galaxy01 = new Galaxy(systemTileList01);
+        galaxy01.getAllPlanetsInGalaxy();
+
     }
+
+    /*
+    public Galaxy buildGame() {
+
+    }
+    */
 }
