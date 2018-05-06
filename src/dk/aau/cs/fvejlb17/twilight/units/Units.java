83a02a5395d14baaa9977198dc3125ea604179b5
cogitantium <falke.cs@gmail.com>
2018-05-06 21:14:35 +0200

    refined scope of Units

--- src/dk/aau/cs/fvejlb17/twilight/units/Units.java
+++ src/dk/aau/cs/fvejlb17/twilight/units/Units.java
@@ -2,7 +2,7 @@ package dk.aau.cs.fvejlb17.twilight.units;
 
 import dk.aau.cs.fvejlb17.twilight.players.Player;
 
-public interface Units {
+interface Units {
 
     Player getOwner();
     int getResourceCost();
