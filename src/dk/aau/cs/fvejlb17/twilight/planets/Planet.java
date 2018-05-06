b99a74d640b74698ca065d945752c66b5306796a
cogitantium <falke.cs@gmail.com>
2018-05-06 16:44:15 +0200

    added overridden equals and hashCode methods to Planet

--- src/dk/aau/cs/fvejlb17/twilight/planets/Planet.java
+++ src/dk/aau/cs/fvejlb17/twilight/planets/Planet.java
@@ -1,5 +1,7 @@
 package dk.aau.cs.fvejlb17.twilight.planets;
 
+import java.util.Objects;
+
 public class Planet {
 
     private String planetName;
@@ -16,5 +18,19 @@ public class Planet {
 
     public int getResourceProduction() {
         return resourceProduction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Planet planet = (Planet) o;
+        return getResourceProduction() == planet.getResourceProduction() &&
+                Objects.equals(getPlanetName(), planet.getPlanetName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPlanetName(), getResourceProduction());
     }
 }
