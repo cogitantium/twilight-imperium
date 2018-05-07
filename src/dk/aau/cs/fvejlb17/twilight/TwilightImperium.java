c7a7c672d22bae8a6b47b2d38c1d30f82ba912a4
cogitantium <falke.cs@gmail.com>
2018-05-07 20:36:48 +0200

    added main call of current functions being implemented

--- src/dk/aau/cs/fvejlb17/twilight/TwilightImperium.java
+++ src/dk/aau/cs/fvejlb17/twilight/TwilightImperium.java
@@ -1,8 +1,8 @@
 package dk.aau.cs.fvejlb17.twilight;
 
 import dk.aau.cs.fvejlb17.twilight.galaxies.*;
+import dk.aau.cs.fvejlb17.twilight.players.Player;
 import dk.aau.cs.fvejlb17.twilight.systems.SystemPosition;
-import dk.aau.cs.fvejlb17.twilight.systems.SystemTile;
 
 class TwilightImperium {
 
@@ -13,6 +13,8 @@ class TwilightImperium {
         System.out.println("check cardinal congruency " + presetGalaxy.checkGalaxyLegality());
 
         System.out.println(SystemPosition.C.toString());
+        Player playerCrassus = new Player("Crassus", "The Emirates of Hacan", "Blue");
+        System.out.println(presetGalaxy.getAllShipsInGalaxyByOwnerSorted(playerCrassus));
 
     }
 }
