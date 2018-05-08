410b1e37c49970f4c44ade42a15e8ad79b32a9d6
cogitantium <falke.cs@gmail.com>
2018-05-08 20:09:12 +0200

    expanded MathsTest

---
+++ tst/dk/aau/cs/fvejlb17/twilight/utilities/MathsTest.java
@@ -0,0 +1,28 @@
+package dk.aau.cs.fvejlb17.twilight.utilities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathsTest {
+
+    @Test
+    void randomIntBetweenBoundsTest1() {
+        //assert that, between two identical runs, that randomBetween is actually between bounds
+        assertTrue((10 <= Maths.randomBetween(10, 20) && Maths.randomBetween(10, 20) <= 20));
+    }
+
+    @Test
+    void randomIntBetweenBoundsTest2() {
+        //assert false that randomBetween is out of bounds from both bounds
+        assertFalse(10 > Maths.randomBetween(10, 20));
+        assertFalse(Maths.randomBetween(10, 20) > 20);
+    }
+
+    @Test
+    void randomIntBetweenBoundsTest3() {
+        //assert that returned integer is inclusive of bounds, specifically if it's either bound
+        int intTest = Maths.randomBetween(5, 6);
+        assertTrue(intTest == 5 ^ intTest == 6);
+    }
+}
