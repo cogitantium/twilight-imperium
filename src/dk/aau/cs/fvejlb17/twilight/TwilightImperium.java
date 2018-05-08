af4101a37047aac0414ded9ea050e6f60f00c024
cogitantium <falke.cs@gmail.com>
2018-05-08 20:14:01 +0200

    added main call for createRandomGame method

--- src/dk/aau/cs/fvejlb17/twilight/TwilightImperium.java
+++ src/dk/aau/cs/fvejlb17/twilight/TwilightImperium.java
@@ -1,18 +1,13 @@
 package dk.aau.cs.fvejlb17.twilight;
 
 import dk.aau.cs.fvejlb17.twilight.galaxies.*;
-import dk.aau.cs.fvejlb17.twilight.players.Player;
-import dk.aau.cs.fvejlb17.twilight.systems.SystemPosition;
-import dk.aau.cs.fvejlb17.twilight.systems.SystemTileList;
-import dk.aau.cs.fvejlb17.twilight.units.UnitList;
-import dk.aau.cs.fvejlb17.twilight.utilities.WriteGameStateToFile;
-
-import java.io.IOException;
 
 class TwilightImperium {
 
     public static void main(String[] args) {
 
         Galaxy presetGalaxy = new GalaxyCreator().createPresetGame();
+
+        Galaxy randomGalaxy = new GalaxyCreator().createRandomGame(6);
     }
 }
