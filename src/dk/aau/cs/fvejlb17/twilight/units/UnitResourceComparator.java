d5569cf076f5d8560e9056fdf5d76df2f452f149
cogitantium <falke.cs@gmail.com>
2018-05-09 01:20:24 +0200

    added needed UnitResourceComparator for resolveSpaceBattle method

---
+++ src/dk/aau/cs/fvejlb17/twilight/units/UnitResourceComparator.java
@@ -0,0 +1,18 @@
+package dk.aau.cs.fvejlb17.twilight.units;
+
+import java.util.Comparator;
+
+public class UnitResourceComparator implements Comparator<Ships> {
+
+    //compares resourceCost ascending
+    @Override
+    public int compare(Ships ship1, Ships ship2) {
+
+        //if resourceCost is equal, return 0 as sorting is finished
+        if (ship1.getResourceCost() == ship2.getResourceCost()) return 0;
+
+        //if ship1 is greater than ship2 in resourceCost, return negative, else positive
+        if (ship1.getResourceCost() > ship2.getResourceCost()) return -1;
+        else return 1;
+    }
+}
